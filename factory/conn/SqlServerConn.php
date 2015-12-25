99cdd1afbffba10309d0d0310033ddfca2333643
nick <nick@nickdeMacBook-Pro.local>
2015-12-25 23:20:37 +0800

    [Add] factory design patterns

---
+++ factory/conn/SqlServerConn.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once "DbConn.php";
+
+class SqlServerConn implements DbConn {
+	public function conn() {
+		echo "連線至SQL Server資料庫";
+		echo "<br />";
+	}
+	public function crud() {
+		echo "於SQL Server資料庫進行CRUD";
+		echo "<br />";
+	}
+}
+
+?>
